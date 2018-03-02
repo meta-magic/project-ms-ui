9a9655fd2ba4dd43215efd6815460c49f1c297c2
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-03-02 14:59:39 +0530

    http: server

--- copyassets.sh
+++ copyassets.sh
@@ -2,5 +2,6 @@
 cp -a src/assets/ dist/
 echo "Copied assets folder to dist"
 echo "Copy dist/ to Apache Server"
-cd dist && npm pack && cp -a projectmsui-0.0.0.tgz /var/www/html/desireplatform/
+cd dist && npm pack &&  mv projectmsui-0.0.0.tgz projectmsui.tgz && cp -a projectmsui.tgz /var/www/html/desireplatform/micro-ui-libs/
+
 
