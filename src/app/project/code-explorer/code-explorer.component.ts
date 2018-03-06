d0281a2a75d371f573cbdfa5fce226d27aa630a4
ashwiniagre1 <ashwini.agre@metamagic.in>
2018-03-06 17:59:28 +0530

    proj creation n code explore added

--- src/app/project/code-explorer/code-explorer.component.ts
+++ src/app/project/code-explorer/code-explorer.component.ts
@@ -2,23 +2,127 @@
  * Created by pratik on 15/2/18.
  */
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'code-explorer',
   template: `
-   <amexio-card>
-     <amexio-header>Code Explorer</amexio-header>
-     <amexio-body>
-       <div style="height: 300px">
-         CODE Explorer here
-       </div>
-     </amexio-body>
-   </amexio-card>
+<amexio-card [header]="true"
+[footer]="false"
+[show]="true">
+    <amexio-header>
+      Project Explore
+    </amexio-header>
+    <amexio-body>
+     <amexio-row>
+            <amexio-column [size]="4">
+                <ng-container *ngIf="fileStructuredata">
+                    <amexio-treeview (nodeClick)="getFileData($event)" [data]="fileStructuredata">
+                        <ng-template #amexioTreeTemplate let-tree let-icon="icon" let-node="node">
+                            <i [attr.class]="node.icon"></i> &nbsp; {{tree.text}}
+                        </ng-template>
+                    </amexio-treeview>
+                </ng-container>
+            </amexio-column>
+            <amexio-column [size]="6">
+                <ng-container *ngIf="isHtml">
+                {{sourceCode}}
+                </ng-container>
+                <ng-container *ngIf="isTypeScript">
+                {{sourceCode}}
+                </ng-container>
+            </amexio-column>
+        </amexio-row>
+    </amexio-body>
+</amexio-card>
 
  `
 })
 export class CodeExplorerComponent implements OnInit {
-  constructor() {}
+  sourceCode: any = '';
 
-  ngOnInit() {}
+  isHtml: boolean;
+
+  isTypeScript: boolean;
+
+  fileStructuredata: any = [];
+
+  publicIpAddress: any;
+
+  constructor(public http: HttpClient) {}
+
+  ngOnInit() {
+    let ipAddress: any;
+    this.http.get('/api/user/person/findLoggedInUserInfo').subscribe(
+      response => {
+        ipAddress = response;
+      },
+      error => {},
+      () => {
+        this.publicIpAddress = ipAddress.response.publicIpAddress;
+        this.getSourceCodeTreeData();
+      }
+    );
+  }
+
+  getSourceCodeTreeData() {
+    let appUrl = 'http://host:9870/projectExplorer/explorer';
+    if (this.publicIpAddress) {
+      appUrl = appUrl.replace('host', this.publicIpAddress);
+    } else {
+      appUrl = appUrl.replace('host', 'localhost');
+    }
+    let filedata: any;
+    this.http.get(appUrl).subscribe(
+      res => {
+        filedata = res;
+      },
+      err => {
+        console.log('Error occured');
+      },
+      () => {
+        if (filedata.response) {
+          let responsedata: any;
+          responsedata = JSON.parse(filedata.response);
+          console.log('filestr', responsedata.children);
+          this.fileStructuredata = responsedata.children;
+        }
+      }
+    );
+  }
+
+  getFileData(data: any) {
+    let appUrl = 'http://host:9870/projectExplorer/findSourceCode';
+    if (this.publicIpAddress) {
+      appUrl = appUrl.replace('host', this.publicIpAddress);
+    } else {
+      appUrl = appUrl.replace('host', 'localhost');
+    }
+    if (data.leaf) {
+      let filedata: any;
+      let sourcePathJSON: any = {};
+      sourcePathJSON['sourcePath'] = data.sourcePath;
+      this.http.post(appUrl, sourcePathJSON).subscribe(
+        res => {
+          filedata = res;
+        },
+        err => {
+          console.log('Error occured');
+        },
+        () => {
+          let responseData = JSON.parse(filedata.response);
+          if (responseData.source) {
+            this.sourceCode = responseData.source;
+            console.log('sourc', responseData.source);
+          }
+          if (responseData.fileType && responseData.fileType === 'html') {
+            this.isHtml = true;
+            this.isTypeScript = false;
+          } else {
+            this.isHtml = false;
+            this.isTypeScript = true;
+          }
+        }
+      );
+    }
+  }
 }
--- src/app/project/create/createproject.component.ts
+++ src/app/project/create/createproject.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { LocalStorageService } from 'platform-commons';
+import { CookieService } from 'platform-commons';
 @Component({
   selector: 'project-create',
   template: `
@@ -11,33 +13,81 @@ import { Component, Input, OnInit } from '@angular/core';
     <amexio-body>
       <amexio-row>
         <amexio-column [size]="6">
-          <amexio-text-input [(ngModel)]="projectname" [field-label]="'Name'" name ="name"
+          <amexio-text-input [(ngModel)]="projectCreationModel.projectName" [field-label]="'Name'" name ="projectCreationModel.projectName"
                             [place-holder]="'Enter name'"
                             [icon-feedback]="true">
           </amexio-text-input>
         </amexio-column>
         <amexio-column [size]="6">
-          <amexio-text-input [field-label]="'Description'" name ="description"
+          <amexio-text-input [field-label]="'Description'" name ="projectCreationModel.projectDescription"
                           [place-holder]="'Enter project description'"
-                          [icon-feedback]="true">
+                          [icon-feedback]="true"
+                          [(ngModel)]="projectCreationModel.projectDescription">
           </amexio-text-input>
-        </amexio-column>      
+        </amexio-column>    
+        <amexio-column [size]="6">
+        <amexio-radio-group
+           [field-label]="'Theme'"
+           name ="theme"
+           [data-reader]="'response'"
+           [display-field]="'themesName'"
+           [value-field]="'themeUUID'"
+           [horizontal]="true"
+           [http-method]="'get'"
+           [http-url]="'/api/project/themes/findAll'"
+           [default-value]="projectCreationModel.themeUUID"  
+           (onSelection)="setTheme($event)">
+        </amexio-radio-group>
+        </amexio-column>  
       </amexio-row>
     </amexio-body>
-    <amexio-action >
-      <amexio-button [label]="'Cancel'" [type]="'secondary'"></amexio-button>
-      <amexio-button [label]="'Save'" [type]="'primary'" (onClick)="saveProject($event)"></amexio-button>
+    <amexio-action>
+    <amexio-button
+    [label]="'save'"
+    [loading]="asyncFlag"
+    [type]="'secondary'"
+    [tooltip]="'save'" 
+    (onClick)="saveProject()">
+    </amexio-button>
     </amexio-action>    
   </amexio-card>
-  
+   <amexio-notification 
+   [data]="msgData"
+   [vertical-position]="'top'"
+   [horizontal-position]="'right'"
+   [auto-dismiss-msg]="true"
+   [auto-dismiss-msg-interval]="7000">
+</amexio-notification>
+<amexio-dialogue [show-dialogue]="isValidateForm" [message-type]="'error'" [closable]="false" [title]="'Error'" [type]="'alert'" [custom]="true">
+<amexio-body>
+    <ol>
+        <li *ngFor="let msgObj of validationMsgArray let index=index">{{msgObj}}</li>
+    </ol>
+</amexio-body>
+<amexio-action>
+    <amexio-button type="primary" (onClick)="okErrorBtnClick()" [label]="'Ok'">
+    </amexio-button>
+</amexio-action>
+</amexio-dialogue>
+
   
 
   `
 })
 export class CreateProjectComponent implements OnInit {
-  projectname = '';
-
-  constructor() {}
+  projectCreationModel: ProjectCreationModel;
+  asyncFlag: boolean = false;
+  newTokenid: string;
+  msgData: any = [];
+  validationMsgArray: any = [];
+  isValidateForm: boolean = false;
+  constructor(
+    private http: HttpClient,
+    private ls: LocalStorageService,
+    private cookieService: CookieService
+  ) {
+    this.projectCreationModel = new ProjectCreationModel();
+  }
 
   ngOnInit() {
     /*  if (localStorage.getItem('project_details')) {
@@ -45,19 +95,101 @@ export class CreateProjectComponent implements OnInit {
     }*/
   }
 
-  saveProject(event: any) {
-    let msg = this.projectname;
-    const message1 = {
-      ms_id: 'project_ms',
-      data: {
-        name: msg
-      }
-    };
-    localStorage.setItem('project_details', JSON.stringify(message1));
-    this.emittProjectSaveEvent(JSON.stringify(message1));
+  // saveProject(event: any) {
+  //   let msg = this.projectname;
+  //   const message1 = {
+  //     ms_id: 'project_ms',
+  //     data: {
+  //       name: msg
+  //     }
+  //   };
+  //   localStorage.setItem('project_details', JSON.stringify(message1));
+  //   this.emittProjectSaveEvent(JSON.stringify(message1));
+  // }
+
+  // emittProjectSaveEvent(string: any) {
+  //   window.postMessage(string, window.location.origin);
+  // }
+  setTheme(themeData: any) {
+    this.projectCreationModel.themeUUID = themeData.themeUUID;
+  }
+  validateFormFields() {
+    debugger;
+    let isValid: boolean = false;
+    this.validationMsgArray = [];
+    if (this.projectCreationModel.projectName == '') {
+      this.validationMsgArray.push('Invalid (Blank) Project Name.');
+    }
+    if (this.projectCreationModel.projectDescription == '') {
+      this.validationMsgArray.push('Invalid (Blank) Project Description.');
+    }
+    if (
+      this.projectCreationModel.themeUUID == null ||
+      this.projectCreationModel.themeUUID == ''
+    ) {
+      this.validationMsgArray.push('Invalid (Blank) Theme.');
+    }
+  }
+  okErrorBtnClick() {
+    this.isValidateForm = false;
+    this.validationMsgArray = [];
   }
 
-  emittProjectSaveEvent(string: any) {
-    window.postMessage(string, window.location.origin);
+  saveProject() {
+    this.validateFormFields();
+    if (this.validationMsgArray && this.validationMsgArray.length >= 1) {
+      this.isValidateForm = true;
+      return;
+    } else {
+      this.isValidateForm = false;
+      this.saveProjectCreation();
+    }
+  }
+  saveProjectCreation() {
+    let response: any;
+    this.asyncFlag = true;
+
+    const requestJson = {
+      projectName: this.projectCreationModel.projectName,
+      projectDescription: this.projectCreationModel.projectDescription,
+      themeUUID: this.projectCreationModel.themeUUID
+    };
+    this.http.post('/api/project/project/save ', requestJson).subscribe(
+      res => {
+        response = res;
+      },
+      err => {
+        this.validationMsgArray.push('Unable to connect to server');
+        this.isValidateForm = true;
+        this.asyncFlag = false;
+      },
+      () => {
+        if (response.success) {
+          this.newTokenid = response.response.tokenid;
+          this.cookieService.set('tokenid', this.newTokenid);
+          this.asyncFlag = false;
+          this.msgData.push(response.successMessage);
+          this.clearData();
+        } else if (!response.success && response.errors) {
+          this.validationMsgArray.push(response.errorMessage);
+          this.asyncFlag = false;
+        }
+      }
+    );
+  }
+
+  clearData() {
+    this.projectCreationModel = new ProjectCreationModel();
+  }
+}
+export class ProjectCreationModel {
+  projectName: string;
+  projectDescription: string;
+  themeUUID: string;
+
+  constructor() {
+    this.projectDescription = '';
+    this.projectName = '';
+    this.themeUUID = '';
   }
 }
