471fec7a1ae25e29fdc9b2bae3c044dace895e82
ashwiniagre1 <ashwini.agre@metamagic.in>
2018-03-27 11:26:42 +0530

    list added

--- src/app/project/create/createproject.component.ts
+++ src/app/project/create/createproject.component.ts
@@ -2,42 +2,83 @@ import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LocalStorageService } from 'platform-commons';
 import { CookieService } from 'platform-commons';
+import { MessagingService } from 'platform-commons';
 import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'project-create',
   template: `
   <amexio-row>
-  <amexio-column [size]="12">
+    <amexio-column [size]="3">
+    <amexio-card [header]="false" [footer]="true" [footer-align]="'right'"
+  [body-height]="82">
+  
+    <amexio-body>
+     <amexio-listbox [enable-checkbox]="false"
+                [header]="'Project List'"
+                [search-placeholder]="'Search Project'"
+                [data]="projectList"
+                [filter]="true"
+                [data-reader]="'response'"
+                [display-field]="'projectName'"
+                (onRowClick)="onProjectSelect($event)">
+</amexio-listbox>
+</amexio-body>
+<amexio-action>
+ <amexio-button
+    [label]="'New'"
+    [type]="'secondary'"
+    [tooltip]="'New'"
+    [size]="'default'" 
+    [icon]="'fa fa-plus fa-lg'"
+    (onClick)="openProjectUi()">
+    </amexio-button>
+</amexio-action>
+</amexio-card>
+    </amexio-column>
+  <amexio-column [size]="9">
+  <ng-container *ngIf="showCard">
   <amexio-card [header]="true" [footer]="true" [footer-align]="'right'"
   [body-height]="82">
     <amexio-header>
-      Project Creation
+             Project Creation          
     </amexio-header>
     <amexio-body>
       <amexio-row>
         <amexio-column [size]="6">
           <amexio-text-input [(ngModel)]="projectCreationModel.projectName" [field-label]="'Name'" name ="projectCreationModel.projectName"
-                            [place-holder]="'Enter name'"
-                            [icon-feedback]="true">
+                            [place-holder]="'Enter Name'"
+                            [icon-feedback]="true"
+                            [disabled]="disblefields">
           </amexio-text-input>
         </amexio-column>
         <amexio-column [size]="6">
-          <amexio-text-input [field-label]="'Description'" name ="projectCreationModel.projectDescription"
-                          [place-holder]="'Enter project description'"
+          <amexio-textarea-input [field-label]="'Description'" name ="projectCreationModel.projectDescription"
+                          [place-holder]="'Enter Description'"
                           [icon-feedback]="true"
+                           [rows]="'1'"
+                           [columns]="'1'"
+                          [disabled]="disblefields"
                           [(ngModel)]="projectCreationModel.projectDescription">
-          </amexio-text-input>
-        </amexio-column>    
+          </amexio-textarea-input>
+        </amexio-column>   
+<ng-container *ngIf="!portDisableFlag">
+         <amexio-column [size]="12">
+
+          Port:{{serverPort}}
+        </amexio-column>   
+                </ng-container>
+
         <amexio-column [size]="6">
        
        
         <amexio-radio-group
            [field-label]="'Amexio Themes'"
-           name ="theme"
+           name ="projectCreationModel.themeUUID"
            [display-field]="'themesName'"
            [value-field]="'themeUUID'"
            [data]="amexioThemes"
-           [(ngModel)]="projectCreationModel.themeUUID"  
+           [disabled]="disblefields"
+           [default-value]="projectCreationModel.themeUUID"
            (onSelection)="setTheme($event)">
         </amexio-radio-group>
       
@@ -45,11 +86,12 @@ import { Router, ActivatedRoute } from '@angular/router';
         <amexio-column [size]="6">
         <amexio-radio-group
            [field-label]="'Material Themes'"
-           name ="theme"
+           name ="projectCreationModel.themeUUID"
            [display-field]="'themesName'"
            [value-field]="'themeUUID'"
            [data]="materialthemes"
-           [(ngModel)]="projectCreationModel.themeUUID"  
+           [disabled]="disblefields"
+           [default-value]="projectCreationModel.themeUUID"
            (onSelection)="setTheme($event)">
         </amexio-radio-group>
         </amexio-column>  
@@ -62,6 +104,8 @@ import { Router, ActivatedRoute } from '@angular/router';
     [type]="'secondary'"
     [tooltip]="'Cancel'"
     [size]="'default'" 
+    [icon]="'fa fa-close'" 
+    [disabled]="disableBtn"
     (onClick)="cancelProject()">
     </amexio-button>
     <amexio-button
@@ -69,11 +113,27 @@ import { Router, ActivatedRoute } from '@angular/router';
     [loading]="asyncFlag"
     [type]="'primary'"
     [tooltip]="'Save'"
-    [size]="'default'" 
+    [size]="'default'"
+    [icon]="'fa fa-save'"  
+    [disabled]="disableBtn"
     (onClick)="saveProject()">
     </amexio-button>
     </amexio-action>    
   </amexio-card>
+  </ng-container>
+   <ng-container *ngIf="!showCard">
+                <amexio-card [header]="true"
+                [footer]="false"
+                [show]="true"
+                [footer-align]="'right'"
+                [body-height]="82">
+                    <amexio-header>
+                     Help Document
+                    </amexio-header>
+                    <amexio-body>
+                    </amexio-body>
+                </amexio-card>
+                </ng-container>
   </amexio-column>
    <amexio-notification 
    [data]="msgData"
@@ -103,33 +163,104 @@ export class CreateProjectComponent implements OnInit {
   serverPort: any;
   newTokenid: string;
   msgData: any = [];
-  projectUUID: string;
+  // projectUUID: string;
   validationMsgArray: any = [];
   isValidateForm: boolean = false;
+  portDisableFlag: boolean = true;
   themes: any;
   amexioThemes: any;
   materialthemes: any;
+  projectList: any;
+  showCard: boolean = false;
+  projectId: string;
+  disblefields: boolean = false;
+  disableBtn: boolean = false;
   constructor(
     private http: HttpClient,
     private ls: LocalStorageService,
     private cookieService: CookieService,
+    private msgService: MessagingService,
     private route: ActivatedRoute
   ) {
     this.projectCreationModel = new ProjectCreationModel();
+    this.themes = [];
     this.amexioThemes = [];
     this.materialthemes = [];
     this.getThemeData();
+    this.getProjectList();
   }
 
-  ngOnInit(): void {
-    this.route.params.forEach(p => {
-      let id = p.id;
-      if (id == null || id == '') {
-        this.projectCreationModel = new ProjectCreationModel();
-      } else {
-        this.getProjData(id);
+  ngOnInit() {}
+
+  //GET PROJECT LIST
+  getProjectList() {
+    let projectDataList: any;
+
+    this.http.get('/api/project/project/findByProjectOwner').subscribe(
+      response => {
+        projectDataList = response;
+      },
+      error => {
+        this.validationMsgArray.push('Unable to connect to server');
+        this.isValidateForm = true;
+      },
+      () => {
+        if (projectDataList.success) {
+          this.projectList = projectDataList;
+        } else {
+          this.validationMsgArray.push(projectDataList.errorMessage);
+          this.isValidateForm = true;
+        }
       }
-    });
+    );
+  }
+
+  openProjectUi() {
+    this.showCard = true;
+    this.portDisableFlag = true;
+    this.disableBtn = false;
+    this.disblefields = false;
+    this.projectCreationModel = new ProjectCreationModel();
+  }
+
+  //GET PROJECT DETAILS OF SELECTED PROJECT IN READ ONLY FORM
+  onProjectSelect(event: any) {
+    let selectProject: any;
+    this.projectCreationModel = new ProjectCreationModel();
+    const projectUUID = event.projectUUID;
+    this.http
+      .get('/api/project/project/selectProject?projectUUID=' + projectUUID)
+      .subscribe(
+        response => {
+          selectProject = response;
+        },
+        err => {
+          this.validationMsgArray.push('Unable to connect to server');
+          this.isValidateForm = true;
+        },
+        () => {
+          if (selectProject.success) {
+            this.showCard = true;
+            this.projectId = selectProject.response.projectUUID;
+            this.projectCreationModel.projectName =
+              selectProject.response.projectName;
+            this.projectCreationModel.projectDescription =
+              selectProject.response.projectDescription;
+            this.projectCreationModel.themeUUID =
+              selectProject.response.themeUUID;
+            this.serverPort = selectProject.response.serverPort;
+            this.portDisableFlag = false;
+            this.newTokenid = selectProject.response.newtokenId;
+            this.cookieService.set('tokenid', this.newTokenid);
+            this.msgService.sendMessage({ projectId: this.projectId });
+            this.disableBtn = true;
+            this.disblefields = true;
+          } else {
+            this.validationMsgArray.push(selectProject.errorMessage);
+            this.isValidateForm = true;
+          }
+        }
+      );
   }
 
   //Set Theme
@@ -200,10 +331,13 @@ export class CreateProjectComponent implements OnInit {
       () => {
         if (response.success) {
           this.newTokenid = response.response.tokenid;
+          this.projectId = response.response.projectUUID;
           this.cookieService.set('tokenid', this.newTokenid);
           this.asyncFlag = false;
           this.msgData.push(response.successMessage);
           this.clearData();
+          this.getProjectList();
+          this.msgService.sendMessage({ projectId: this.projectId });
         } else {
           if (response.errorMessage == null) {
             this.validationMsgArray.push(response.errors);
@@ -223,31 +357,7 @@ export class CreateProjectComponent implements OnInit {
     this.projectCreationModel = new ProjectCreationModel();
   }
 
-  //To fetch Project Details
-  getProjData(projectUUID: any) {
-    let projectData: any;
-    this.projectCreationModel = new ProjectCreationModel();
-    this.http
-      .get('/api/project/project/findByProjectUUID?projectUUID=' + projectUUID)
-      .subscribe(
-        response => {
-          projectData = response;
-        },
-        err => {
-          console.log('Error occured');
-        },
-        () => {
-          this.projectCreationModel.projectName =
-            projectData.response.projectName;
-          this.projectCreationModel.projectDescription =
-            projectData.response.projectDescription;
-          this.projectCreationModel.themeUUID = projectData.response.themeUUID;
-          this.serverPort = projectData.response.serverPort;
-        }
-      );
-  }
-
-  //To Fetch Theme Data from DB
+  // To Fetch Theme Data from DB
   getThemeData() {
     let response: any;
     this.http.get('/api/project/themes/findAll').subscribe(
@@ -295,11 +405,11 @@ export class CreateProjectComponent implements OnInit {
 export class ProjectCreationModel {
   projectName: string;
   projectDescription: string;
-  themeUUID: string;
+  themeUUID: any;
 
   constructor() {
     this.projectDescription = '';
     this.projectName = '';
-    this.themeUUID = '';
+    this.themeUUID = null;
   }
 }
