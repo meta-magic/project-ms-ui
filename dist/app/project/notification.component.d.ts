458777d93b2d89bed1c04ee913d341ed63468cd9
datsgawas <dattaram.gawas@metamagic.in>
2018-09-20 16:48:52 +0530

    dist folder removed

--- dist/app/project/notification.component.d.ts
+++
@@ -1,7 +0,0 @@
-import { OnInit } from '@angular/core';
-import { NotificationService } from 'platform-commons';
-export declare class ProjectNotificationComponent implements OnInit {
-    _notificationService: NotificationService;
-    constructor(_notificationService: NotificationService);
-    ngOnInit(): void;
-}
--- dist/project-ms-ui.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './index';
-export { TabcodeComponent as ɵa } from './app/project/code-explorer/tabcode.component';
